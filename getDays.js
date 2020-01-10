4d80e545fc7cf3b24064246fe7e3b4aa3e203792
Roshan Ghale <ghaleroshan@gmail.com>
2020-01-10 18:01:58 +1100

    moves logic inside a function for testing purpose

--- getDays.js
+++ getDays.js
@@ -1,23 +1,22 @@
 const msPerDay = 1000 * 60 * 60 * 24;
-const date = new Date();
 const validMinDate = new Date("1901/01/01").getTime();
 const validMaxDate = new Date("2999/12/31").getTime();
 
-process.argv.forEach(() => {
-  const args = process.argv.slice(2);
-  const startDate = args[0];
-  const endDate = args[1];
+exports.diffDays = () => {
+  const startDate = process.argv[2];
+  const endDate = process.argv[3];
   const startingDate = new Date(startDate).getTime();
   const endingDate = new Date(endDate).getTime();
-  if (startDate !== endDate) {
-    if (startingDate > validMinDate && endingDate < validMaxDate) {
+  if (startingDate > validMinDate && endingDate < validMaxDate) {
+    if (startingDate !== endingDate) {
       const diff = endingDate - startingDate;
       const noOfWorkingDays = Math.floor((diff - 1) / msPerDay);
-      console.log(noOfWorkingDays);
-    } else {
-      console.log("Please enter valid dates");
+      console.log(`The number of elapsed days are: ${noOfWorkingDays} days`);
+      process.exit(0);
     }
+    console.log("The number of elapsed days are 0");
   } else {
-    console.log("The working day is 0");
+    console.log("Please enter Valid Days");
   }
-});
+};
+console.log(exports.diffDays());
--- test/getDaysTest.js
+++ test/getDaysTest.js
@@ -1,8 +1,8 @@
 const getDays = require("../getDays");
 
+console.log(getDays.diffDays());
+
 test("Should return the number of days between the two dates", () => {
-  let startDate = "06/02/1983";
-  let endDate = "06/22/1983";
-  let res = getDays(startDate, endDate);
-  expect(res).toEqual(19);
+  let res = getDays.diffDays("06/02/1983", "06/22/1983");
+  expect(res).toEqual(`The number of elapsed days are: ${res} days`);
 });
